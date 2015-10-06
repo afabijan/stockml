8adda6b5ed29d7f4cc220418a10142598a44c62d
afabijan <aleksander.fabijan@me.com>
2015-10-06 14:02:19 +0200

    added error hadnling for fuzzy values

--- less.py
+++ less.py
@@ -3,6 +3,14 @@ import os
 import time
 from datetime import datetime
 
+from time import mktime
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib import style
+style.use("dark_background")
+import re
+
+
 path = "/Users/ae9693/Desktop/Programming/python/yt/intraQuarter"
 def Key_Stats(gather = "Total Debt/Equity (mrq)"):
     statspath = path +('/_KeyStats')
@@ -14,11 +22,13 @@ def Key_Stats(gather = "Total Debt/Equity (mrq)"):
                                  'Price',
                                  'stock_p_change',
                                  'SP500',
-                                 'sp500_p_change'])
+                                 'sp500_p_change',
+                                 'Difference']
+                                 )
 
     sp500_df = pd.DataFrame.from_csv('YAHOO-INDEX_GSPC.csv')
     ticker_list = list()
-    for folder in stock_list[1:15]:
+    for folder in stock_list[1:50]:
         each_file = os.listdir(folder)
 
         ticker = folder.split("/")[-1]
@@ -37,8 +47,11 @@ def Key_Stats(gather = "Total Debt/Equity (mrq)"):
                 # print full_file_path
                 source = open(full_file_path, 'r').read()  # opening a file for reading
                 try:
-                    value = float(source.split(gather+':</td><td class="yfnc_tabledata1">')[1].split('</td>')[0])
-
+                    try:
+                        value = float(source.split(gather+':</td><td class="yfnc_tabledata1">')[1].split('</td>')[0])
+                    except Exception as ee:
+                        value = float(source.split(gather+':</td>\n<td class="yfnc_tabledata1">')[1].split('</td>')[0])
+                        #print ee, file
                     try:
                         sp500_date = datetime.fromtimestamp(unix_time).strftime('%Y-%m-%d')
                         row = sp500_df[(sp500_df.index == sp500_date)]
@@ -47,10 +60,21 @@ def Key_Stats(gather = "Total Debt/Equity (mrq)"):
                         sp500_date = datetime.fromtimestamp(unix_time-259200).strftime('%Y-%m-%d')
                         row = sp500_df[(sp500_df.index == sp500_date)]
                         sp500_value = float(row["Adjusted Close"])
+                    try:
+                        stock_price = float(source.split('</small><big><b>')[1].split('</b></big>')[0])
+                    except Exception as e:
+                        # coverin exceptions that look like <span id="yfs_l10_alxn">57.13</span>
+                        try:
+                            stock_price = stock_price = source.split('</small><big><b>')[1].split('</b></big>')[0]
+                            stock_price = re.search(r'(\d{1,8}\.\d{1,8})',stock_price)
+                            stock_price = float(stock_price.group(1))
+                        except Exception as ex:
+                            print "hit exception :(", e
+                            pass
 
-                    stock_price = float(source.split('</small><big><b>')[1].split('</b></big>')[0])
-
-                    print "stock_price:", stock_price, "ticker:", ticker
+                        # print e,  stock_price
+                        #print ticker, e, file
+                    # print "stock_price:", stock_price, "ticker:", ticker
 
 
                     # handle if
@@ -74,12 +98,28 @@ def Key_Stats(gather = "Total Debt/Equity (mrq)"):
                                     'Price': stock_price,
                                     'stock_p_change': stock_p_change,
                                     'SP500': sp500_value,
-                                    'sp500_p_change': sp500_p_change
+                                    'sp500_p_change': sp500_p_change,
+                                    'Difference': stock_p_change - sp500_p_change
                                     }, ignore_index=True)
 
                 except Exception as e:
                     # print e
                     pass
+
+    for each_ticker in ticker_list:
+        try:
+            plot_df = df[(df['Ticker'] == each_ticker)]
+            plot_df = plot_df.set_index(['Date'])
+
+            plot_df['Difference'].plot(label=each_ticker)
+            plt.legend()
+
+
+        except:
+            pass
+
+
+    plt.show()
     save = gather.replace(' ', '').replace('(', '').replace(')', '').replace('/', '')+'.csv'
     #print(save)
     df.to_csv(save)
